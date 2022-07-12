82821bbdf129a5677304dc3bbb1cce9f94cfce82
RISHABH SHARMA <85387033+Rishabh2134@users.noreply.github.com>
2022-07-12 20:23:23 +0530

    Create dbinfo.php

---
+++ dbinfo.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_SERVER', 'mysql-database.cr73uuf5b5iu.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', '12345678');
+define('DB_DATABASE', 'demo');
+
+?>
+                
